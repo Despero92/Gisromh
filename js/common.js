ffbaa306dec67ac970c832009b4d82aa4eafb9d8
Denis Chernishev <denis.despero@gmail.com>
2017-06-02 12:08:12 +0300

    first commit in this repository

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(document).ready( function(){
+    $('.bxslider').bxSlider({
+        pagerCustom: '#bx-pager'
+    });
+});
+$(document).ready( function(){
+    $('.tlt').textillate({
+        loop: false
+
+    });
+});
