2d5d12277b1d5b852565ada14b1bedaa3dcffdd5
Denis Chernishev <denis.despero@gmail.com>
2017-06-14 01:16:17 +0300

    add animation on page

--- js/common.js
+++ js/common.js
@@ -1,11 +1,25 @@
 $(document).ready( function(){
-    $('.bxslider').bxSlider({
-        pagerCustom: '#bx-pager'
+    $(" #title-1 ").textillate({
+        loop: true,
+        minDisplayTime: 6000,
+        initialDelay: 3000,
+        in:{
+            effect: "fadeInRight",
+            delay: 100,
+            shuffle: true,
+            sequence: true
+        },
+        out:{
+            effect: "fadeInLeft",
+            delay: 100,
+            sequence: true
+        }
     });
+    $(" #title-2 ").animated("fadeInDown", "fadeOutUp");
+    $(" #title-3 ").animated("fadeInLeft", "fadeOutRight");
+    $(" #title-4 ").animated("fadeInLeft", "fadeOutRight");
+    $(" #statistic-box-1 ").animated("fadeInLeft", "fadeOutLeft");
+    $(" #statistic-box-2 ").animated("fadeInDown", "fadeOutUp");
+    $(" #statistic-box-3 ").animated("fadeInRight", "fadeOutRight");
 });
-$(document).ready( function(){
-    $('.tlt').textillate({
-        loop: false
 
-    });
-});
